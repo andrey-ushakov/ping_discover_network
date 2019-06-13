f102be2dd87472507f577003f2d0ed441598fbf6
Andrey <ushakov.andrey.34@gmail.com>
2019-06-13 23:26:59 +0200

    Added port_range example.

--- example/main.dart
+++ example/main.dart
@@ -6,8 +6,9 @@
  */
 import 'package:ping_discover_network/ping_discover_network.dart';
 
+/// Discover available network devices in a given subnet on a given port
 void main() {
-  final stream = NetworkAnalyzer.discover('192.168.0', 80);
+  final stream = NetworkAnalyzer.discover('192.168.0', 9100);
 
   stream.listen((NetworkAddress addr) {
     if (addr.exists) {
---
+++ example/port_range.dart
@@ -0,0 +1,31 @@
+/*
+ * ping_discover_network
+ * Created by Andrey Ushakov
+ * 
+ * See LICENSE for distribution and usage details.
+ */
+import 'package:ping_discover_network/ping_discover_network.dart';
+
+void checkPortRange(String subnet, int fromPort, int toPort) {
+  if (fromPort > toPort) {
+    return;
+  }
+
+  print('port $fromPort');
+  final stream = NetworkAnalyzer.discover(subnet, fromPort);
+
+  stream.listen((NetworkAddress addr) {
+    if (addr.exists) {
+      print('Found device: ${addr.ip}:$fromPort');
+    }
+  }).onDone(() {
+    if (fromPort + 1 <= toPort) {
+      checkPortRange(subnet, fromPort + 1, toPort);
+    }
+  });
+}
+
+/// Discover available network devices in a given subnet on a given port range
+void main() {
+  checkPortRange('192.168.0', 400, 410);
+}
