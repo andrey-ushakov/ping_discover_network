31240bbaca54e6daad7df33764246ea3c98c68dc
Andrey <ushakov.andrey.34@gmail.com>
2019-06-16 23:08:59 +0200

    Added Flutter example

--- example/flutter_example/lib/main.dart
+++ example/flutter_example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ping_discover_network/ping_discover_network.dart';
+import 'package:wifi/wifi.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,51 +12,151 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String localIp = '';
+  List<String> devices = [];
+  bool isDiscovering = false;
+  int found = -1;
+  TextEditingController portController = TextEditingController(text: '80');
 
-  void _incrementCounter() {
+  void discover(BuildContext ctx) async {
     setState(() {
-      _counter++;
+      isDiscovering = true;
+      devices.clear();
+      found = -1;
     });
+
+    String ip;
+    try {
+      ip = await Wifi.ip;
+      print('local ip:\t$ip');
+    } catch (e) {
+      final snackBar = SnackBar(
+          content: Text('WiFi is not connected', textAlign: TextAlign.center));
+      Scaffold.of(ctx).showSnackBar(snackBar);
+      return;
+    }
+    setState(() {
+      localIp = ip;
+    });
+
+    final String subnet = ip.substring(0, ip.lastIndexOf('.'));
+    int port = 80;
+    try {
+      port = int.parse(portController.text);
+    } catch (e) {
+      portController.text = port.toString();
+    }
+    print('subnet:\t$subnet, port:\t$port');
+
+    final stream = NetworkAnalyzer.discover(subnet, port);
+
+    stream.listen((NetworkAddress addr) {
+      if (addr.exists) {
+        print('Found device: ${addr.ip}');
+        setState(() {
+          devices.add(addr.ip);
+          found = devices.length;
+        });
+      }
+    })
+      ..onDone(() {
+        setState(() {
+          isDiscovering = false;
+          found = devices.length;
+        });
+      })
+      ..onError((dynamic e) {
+        final snackBar = SnackBar(
+            content: Text('Unexpected exception', textAlign: TextAlign.center));
+        Scaffold.of(ctx).showSnackBar(snackBar);
+      });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Discover Local Network'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+      body: Builder(
+        builder: (BuildContext context) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: portController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Port',
+                    hintText: 'Port',
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text('Local ip: $localIp', style: TextStyle(fontSize: 16)),
+                SizedBox(height: 15),
+                RaisedButton(
+                    child: Text(
+                        '${isDiscovering ? 'Discovering...' : 'Discover'}'),
+                    onPressed: isDiscovering ? null : () => discover(context)),
+                SizedBox(height: 15),
+                found >= 0
+                    ? Text('Found: $found device(s)',
+                        style: TextStyle(fontSize: 16))
+                    : Container(),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: devices.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: <Widget>[
+                          Container(
+                            height: 60,
+                            padding: EdgeInsets.only(left: 10),
+                            alignment: Alignment.centerLeft,
+                            child: Row(
+                              children: <Widget>[
+                                Icon(Icons.devices),
+                                SizedBox(width: 10),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        '${devices[index]}:${portController.text}',
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Icon(Icons.chevron_right),
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                        ],
+                      );
+                    },
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          );
+        },
       ),
     );
   }
