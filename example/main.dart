ec251ec658a4bd86203f0c2455258cedde798c0c
Andrey <ushakov.andrey.34@gmail.com>
2019-09-05 18:17:15 +0200

    Update example.

--- example/main.dart
+++ example/main.dart
@@ -8,16 +8,19 @@ import 'package:ping_discover_network/ping_discover_network.dart';
 
 /// Discover available network devices in a given subnet on a given port
 void main() async {
+  // NetworkAnalyzer.discover pings PORT:IP one by according to timeout.
+  // NetworkAnalyzer.discover2 pings all PORT:IP addresses at once.
+
   const port = 80;
-  final stream = NetworkAnalyzer.discover(
+  final stream = NetworkAnalyzer.discover2(
     '192.168.0',
     port,
-    timeout: Duration(milliseconds: 400),
+    timeout: Duration(milliseconds: 5000),
   );
 
   int found = 0;
   stream.listen((NetworkAddress addr) {
-    print('${addr.ip}:$port');
+    // print('${addr.ip}:$port');
     if (addr.exists) {
       found++;
       print('Found device: ${addr.ip}:$port');
